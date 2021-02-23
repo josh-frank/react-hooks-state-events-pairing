632d46e162d45f08f5e4e2bbc6865cb6ddab5258
mcardona9015 <mcardona9015@gmail.com>
2021-02-23 14:58:10 -0500

    search deliverable finished!

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,20 +1,27 @@
 import React, { useState } from "react";
 
 function Comments({ comments }) {
-
+    // const commentVoteObject = comments.map()
     const [ isHidden, toggleHidden ] = useState( false );
     const [ currentComments, updateComments ] = useState( comments );
-
+    const [ searchQuery, updateSearchQuery ] = useState('')
+    console.log('searchQuery: ', searchQuery);
+    
     function deleteComment( commentToDeleteId ) {
         const updatedComments = currentComments.filter( comment => comment.id !== commentToDeleteId );
         updateComments( updatedComments );
     }
-
+    
     function hide() {
         toggleHidden( isHidden => !isHidden );
     }
-
-    const commentList = currentComments.map(comment => {
+    
+    // function incrementUpvotes(){ updateUpvotes( numberOfUpvotes + 1 ) }
+    // function incrementDownvotes(){ updateDownvotes( numberOfDownvotes + 1 ) }
+    // const [ numberOfUpvotes, updateUpvotes ] = useState( 0 );
+    // const [ numberOfDownvotes, updateDownvotes ] = useState( 0 );
+    
+    const commentList = filterComments().map(comment => {
         return (
             <div key={comment.id}  className="comment">
                 <b>
@@ -23,13 +30,25 @@ function Comments({ comments }) {
                 </b>
                 <p>
                     {comment.comment}
+                    {/* <button className="upvote-button" onClick={ incrementUpvotes }>
+                        {numberOfUpvotes} 👍
+                    </button>
+                    <button className="downvote-button" onClick={ incrementDownvotes }>
+                        {numberOfDownvotes} 👎
+                    </button> */}
                 </p>
             </div>
         )
     })
 
+    function filterComments() {
+        return currentComments.filter(comment => comment.user.toLowerCase().includes(searchQuery.toLowerCase()))
+        
+    }
+
     const commentsSection = (
         <div className="comments-section">
+            <input className="search-bar" placeholder="Search comments" onChange={(e) => updateSearchQuery(e.target.value)}/>
             <h3>
             {commentList.length} Comments 
             </h3>
